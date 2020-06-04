9c192c85902441071e9c8eb4dcd3721b0e7bdb94
Chris Yoon <chy.yoon@gmail.com>
2020-06-04 11:30:42 -0700

    add submit button Co-authored-by: Jeff Kim <kim2jy@hotmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,21 @@
 //Businasty logic
-var height = feetInput * 12 + inchInput;
 
 //Interface logic
 $(document).ready(function() {
-  $("#formOne").submit(function() {
+  $("#form-one").submit(function(event) {
     event.preventDefault();
     const feetInput = parseInt($("#height-feet").val());
     const inchInput = parseInt($("#height-inches").val());
-    
+    const height = feetInput * 12 + inchInput;
+
     if (height < 60) {
-      (#too-short).show();
+      $("#too-short").show();
     }
     else if (height > 80) {
-      (#too-height).show();
+      $("#too-tall").show();
     }
     else {
-      (#just-right).show();
+      $("#just-right").show();
     }      
   });
 });
